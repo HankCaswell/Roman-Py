6509d42d1742d8de82eb94068df0e72859542af6
Hank Caswell <david.h.caswell@proton.me>
2024-02-01 17:43:12 -0500

    passed all tests

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,35 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    output = ""
+    roman_numerals = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1,
+    }
+
+    keyss = list(roman_numerals.keys())
+
+    for key in keyss:
+        current_value = roman_numerals[key]
+
+        # print(f"key = {key}")
+        # print(f"value = {current_value}")
+
+        while num >= current_value:
+            output += key
+            num -= current_value
+
+    return output
+
+
+print(to_roman(4))
+print(to_roman(26))
--- test_roman_numerals.py
+++ test_roman_numerals.py
@@ -10,7 +10,15 @@ def test_02_multiple_entries():
 def test_03_odd_numerals():
     assert to_roman(4) == 'IV'
 
-def test_04_all_edge_cases():
+def test_04():
+    assert to_roman(44) == 'XLIV'
+
+def test_05_all_edge_cases():
     assert to_roman(944) == 'CMXLIV'
+    assert to_roman(0) == ''
+
+
+
+
     
 # add tests to cover different edge cases
